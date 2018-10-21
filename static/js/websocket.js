7c49b549f988e64cd15d82e9b7e8ca17158b2573
s-iritani <39664478+s-iritani@users.noreply.github.com>
2018-10-21 12:39:57 +0900

    Refactoring  websocket.js

--- static/js/websocket.js
+++ static/js/websocket.js
@@ -1,3 +1,6 @@
+const KEY_CODE_ENTER = 13;
+const STRING_ENMPTY = "";
+
 let ws = new WebSocket("ws://localhost:8888/ws");
 ws.onmessage = function(evt) {
   let msg = document.createElement('p');
@@ -6,21 +9,25 @@ ws.onmessage = function(evt) {
 };
 
 function sendMessage()  {
-  if (document.getElementById('msg').value != "") {
+  if (document.getElementById('msg').value !== STRING_ENMPTY) {
     let m = document.getElementById('msg');
     ws.send(m.value);
-    document.getElementById('msg').value = "";
+    document.getElementById('msg').value = STRING_ENMPTY;
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById('send').addEventListener('click', sendMessage);
-  document.getElementById('msg').addEventListener('keydown', handleKeydown);
-}, false);
+function handleClick() {
+  sendMessage();
+}
 
 function handleKeydown(e){
   let keyCode = e.keyCode;
-  if (keyCode == 13) {
+  if (keyCode === KEY_CODE_ENTER) {
     sendMessage();
   }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('send').addEventListener('click', handleClick);
+  document.getElementById('msg').addEventListener('keydown', handleKeydown);
+}, false);
